da96ecd41d202031feff84ce1f76c4f02d0f3e85
ifazalam1 <ifaz.alam1@gmail.com>
2024-03-26 17:45:11 +0600

    user id added to custom template category and custom template

---
+++ database/migrations/2024_03_26_114147_add_new_field_to_custom_template_categories_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('custom_template_categories', function (Blueprint $table) {
+            $table->unsignedBigInteger('user_id')->nullable(); // Change 'string' to the appropriate data type
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('custom_template_categories', function (Blueprint $table) {
+            $table->dropColumn('user_id');
+        });
+    }
+};
