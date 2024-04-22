110ffe27c317b0ee81b7ed181d2b176fae22758a
ifazalam1 <ifaz.alam1@gmail.com>
2024-04-22 13:35:56 +0600

    PT240422073538

--- resources/views/user/layouts/topbar.blade.php
+++ resources/views/user/layouts/topbar.blade.php
@@ -165,6 +165,9 @@
                         <!-- item-->
                         <h6 class="dropdown-header">Welcome!</h6>
                         <a class="dropdown-item" href="{{ route('edit.profile') }}"><i class="mdi mdi-account-circle text-muted fs-16 align-middle me-1"></i> <span class="align-middle">Profile</span></a>
+                        
+                        <a class="dropdown-item" href="{{route('user.dashboard')}}" target="_blank"><i class="mdi mdi-home text-muted fs-16 align-middle me-1"></i> <span class="align-middle">Dashboard</span></a>
+
                         <a class="dropdown-item" href="{{route('home')}}" target="_blank"><i class="mdi mdi-message-text-outline text-muted fs-16 align-middle me-1"></i> <span class="align-middle">Front-End</span></a>
                        
                         <div class="dropdown-divider"></div>
