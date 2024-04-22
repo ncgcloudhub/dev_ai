2ceac9b4509a303735f5a2b54989294d7f70d724
ifazalam1 <ifaz.alam1@gmail.com>
2024-04-22 15:16:53 +0600

    PT240422091600

---
+++ app/Models/PromptLibraryCategory.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PromptLibraryCategory extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+}
