f54f25e3b0c5cf91b9289a417126b6f46fad1ec0
ifazalam1 <ifaz.alam1@gmail.com>
2024-04-24 15:45:08 +0600

    PT240424094451

--- resources/views/backend/template/template_view.blade.php
+++ resources/views/backend/template/template_view.blade.php
@@ -73,89 +73,86 @@
                                         <div class="accordion-body">
 
             
-                <div class="col-md-12">
-                    <label for="max_result_length" class="form-label">Max Result Length</label>
-                    <input type="range" name="max_result_length" class="form-range" id="max_result_length" min="10" max="4000" step="10" value="100">
-                    <input type="number" name="max_result_length_value" class="form-control" id="max_result_length_value" min="10" max="4000" step="10" value="100">
-                    
-                </div>
+                                            <div class="col-md-12">
+                                                <label for="max_result_length" class="form-label">Max Result Length</label>
+                                                <input type="range" name="max_result_length" class="form-range" id="max_result_length" min="10" max="4000" step="10" value="100">
+                                                <input type="number" name="max_result_length_value" class="form-control" id="max_result_length_value" min="10" max="4000" step="10" value="100">
+                                                
+                                            </div>
                 
 
-                <div class="row">
-                    <div class="col-md-6">
-                        <label for="creative_level" class="form-label">Creative Level</label>
-                        <select class="form-select" name="creative_level" id="creative_level" aria-label="Floating label select example" onchange="disableInputs()">
-                            <option disabled selected="">No Creativity Level</option>
-                            <option value="High">High</option>
-                            <option value="Medium">Medium</option>
-                            <option value="Low">Low</option>
-                        </select>
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <label for="creative_level" class="form-label">Creative Level</label>
+                                                    <select class="form-select" name="creative_level" id="creative_level" aria-label="Floating label select example" onchange="disableInputs()">
+                                                        <option disabled selected="">No Creativity Level</option>
+                                                        <option value="High">High</option>
+                                                        <option value="Medium">Medium</option>
+                                                        <option value="Low">Low</option>
+                                                    </select>
+                                                </div>
+
+                                                <div class="col-md-6">
+                                                    <label for="tone" class="form-label">Choose a Tone</label>
+                                                    <select class="form-select" name="tone" id="tone" aria-label="Floating label select example">
+                                                        <option disabled selected="">Enter Tone</option>
+                                                        <option value="Friendly">Friendly</option>
+                                                        <option value="Luxury">Luxury</option>
+                                                        <option value="Relaxed">Relaxed</option>
+                                                        <option value="Professional">Professional</option>
+                                                        <option value="Casual">Casual</option>
+                                                        <option value="Excited">Excited</option>
+                                                        <option value="Bold">Bold</option>
+                                                        <option value="Masculine">Masculine</option>
+                                                        <option value="Dramatic">Dramatic</option>
+                                                
+                                                    </select>
+                                                
+                                                </div>
+                                            </div>
+
+                                            <div class="row">
+
+                                                <div class="col-md-6">
+                                                    <label for="temperature" class="form-label">Temperature (Creativity)</label>
+                                                    <input type="range" name="temperature" class="form-range" id="temperature" min="0" max="1" step="0.01" value="0.00" >
+                                                    <input type="number" name="temperature_value" class="form-control" id="temperature_value" min="0" max="1" step="0.01" value="0.00">
+                                                </div>
+                                                
+                                                <div class="col-md-6">
+                                                    <label for="top_p" class="form-label">Top P</label>
+                                                    <input type="range" name="top_p" class="form-range" id="top_p" min="0" max="1" step="0.01" value="1.00" >
+                                                    <input type="number" name="top_p_value" class="form-control" id="top_p_value" min="0" max="1" step="0.01" value="1.00">
+                                                </div>
+                                            </div>
+
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <label for="frequency_penalty" class="form-label">Frequency Penalty</label>
+                                                    <input type="range" name="frequency_penalty" class="form-range" id="frequency_penalty" min="0" max="2" step="0.01" value="0.00">
+                                                    <input type="number" name="frequency_penalty_value" class="form-control" id="frequency_penalty_value" min="0" max="2" step="0.01" value="0.00">
+
+                                                </div>
+
+                                                <div class="col-md-6">
+                                                    <label for="presence_penalty" class="form-label">Presence Penalty</label>
+                                                    <input type="range" name="presence_penalty" class="form-range" id="presence_penalty" min="0" max="2" step="0.01" value="0.00">
+                                                    <input type="number" name="presence_penalty_value" class="form-control" id="presence_penalty_value" min="0" max="2" step="0.01" value="0.00">
+
+
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                    <div class="col-12">
+                        <div class="text-end">
+                            <button class="btn btn-rounded btn-primary mb-5">Generate</button>
+                            {{-- <input type="submit" class="btn btn-rounded btn-primary mb-5" value="Generate"> --}}
+                        </div>
                     </div>
-
-                    <div class="col-md-6">
-                        <label for="tone" class="form-label">Choose a Tone</label>
-                        <select class="form-select" name="tone" id="tone" aria-label="Floating label select example">
-                            <option disabled selected="">Enter Tone</option>
-                            <option value="Friendly">Friendly</option>
-                            <option value="Luxury">Luxury</option>
-                            <option value="Relaxed">Relaxed</option>
-                            <option value="Professional">Professional</option>
-                            <option value="Casual">Casual</option>
-                            <option value="Excited">Excited</option>
-                            <option value="Bold">Bold</option>
-                            <option value="Masculine">Masculine</option>
-                            <option value="Dramatic">Dramatic</option>
-                    
-                          </select>
-                    
-                    </div>
-                </div>
-
-                <div class="row">
-
-                    <div class="col-md-6">
-                        <label for="temperature" class="form-label">Temperature (Creativity)</label>
-                        <input type="range" name="temperature" class="form-range" id="temperature" min="0" max="1" step="0.01" value="0.00" >
-                        <input type="number" name="temperature_value" class="form-control" id="temperature_value" min="0" max="1" step="0.01" value="0.00">
-                    </div>
-                    
-                    <div class="col-md-6">
-                        <label for="top_p" class="form-label">Top P</label>
-                        <input type="range" name="top_p" class="form-range" id="top_p" min="0" max="1" step="0.01" value="1.00" >
-                        <input type="number" name="top_p_value" class="form-control" id="top_p_value" min="0" max="1" step="0.01" value="1.00">
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-md-6">
-                        <label for="frequency_penalty" class="form-label">Frequency Penalty</label>
-                        <input type="range" name="frequency_penalty" class="form-range" id="frequency_penalty" min="0" max="2" step="0.01" value="0.00">
-                        <input type="number" name="frequency_penalty_value" class="form-control" id="frequency_penalty_value" min="0" max="2" step="0.01" value="0.00">
-
-                    </div>
-
-                    <div class="col-md-6">
-                        <label for="presence_penalty" class="form-label">Presence Penalty</label>
-                        <input type="range" name="presence_penalty" class="form-range" id="presence_penalty" min="0" max="2" step="0.01" value="0.00">
-                        <input type="number" name="presence_penalty_value" class="form-control" id="presence_penalty_value" min="0" max="2" step="0.01" value="0.00">
-
-
-
-                    </div>
-                </div>
-
-
-            </div>
-        </div>
-    </div>
-    
-</div>
-<div class="col-12">
-    <div class="text-end">
-        <button class="btn btn-rounded btn-primary mb-5">Generate</button>
-        {{-- <input type="submit" class="btn btn-rounded btn-primary mb-5" value="Generate"> --}}
-    </div>
-</div>
                         </form>
                     </div>
                     
@@ -165,8 +162,8 @@
            <div class="col">
 
             <!-- Add the Download Content button -->
-<button id="downloadButton" class="btn btn-success">Download Content</button>
-<button id="copyButton" class="btn btn-primary">Copy Content</button> <!-- Added button -->
+            <button id="downloadButton" class="btn btn-success">Download Content</button>
+            <button id="copyButton" class="btn btn-primary">Copy Content</button> <!-- Added button -->
 
             <div class="row mt-2">
                 <div class="col-lg-12">
@@ -188,6 +185,16 @@
 
  
 </div>
+
+
+<div class="text-end">
+    @if($Template->slug == 'image-prompt-idea') {{-- Assuming template_id for the specific template is 78 --}}
+        <!-- Add the Generate Image button -->
+        <a href="{{route('generate.image.view')}}" id="downloadButton" class="btn btn-warning">Generate Image Form</a>
+    @endif
+</div>
+
+
 @endsection
 @section('script')
 <script src="{{ URL::asset('build/libs/@ckeditor/ckeditor5-build-classic/build/ckeditor.js') }}"></script>
