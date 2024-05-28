f946b84d0a1767e20920929e0e0135efdade93aa
ifazalam1 <ifaz.alam1@gmail.com>
2024-05-29 00:51:06 +0600

    frontend template view updated

--- resources/views/frontend/template.blade.php
+++ resources/views/frontend/template.blade.php
@@ -15,35 +15,54 @@
         <!-- Begin page -->
         <div class="layout-wrapper landing">
            @include('frontend.body.nav_frontend')
-
+          
            <br><br>
 <div class="container mt-5">
-    <div class="row justify-content-center"> 
-<div class="row">
-    <div class="col-12">
-        <div class="row row-cols-xxl-5 row-cols-lg-3 row-cols-1">
-            @foreach ($templates as $item)       
-            <div class="col">
-                <a href="{{ route('frontend.free.template.view', ['slug' => $item->slug]) }}">
-                <div class="card card-body">
-                    <div class="d-flex mb-4 align-items-center">
-                        <div class="flex-shrink-0">
-                            <img src="/build/images/templates/{{$item->icon}}.png" alt="" class="avatar-sm rounded-circle">
+    <div class="row template-row">
+        @foreach ($templates as $item)
+       
+        <div class="col-md-3 p-3 template-card" data-category="{{$item->category_id}}" data-search="{{ strtolower($item->template_name . ' ' . $item->description) }}">
+            
+           
+                <div class="card" style="box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);">
+                    <a href="{{ route('template.view', ['slug' => $item->slug]) }}">
+                    <div class="card-body">
+                        <div style="width: 42px; height: 42px; border-radius: 50%; background-color: #ffffff; display: flex; align-items: center; justify-content: center; box-shadow: 0 .125rem .3rem -0.0625rem rgba(0,0,0,.1),0 .275rem .75rem -0.0625rem rgba(249,248,249,.06)">
+                            {{-- <i style="font-size: 24px; color: #333;" class="{{$item->icon}}"></i> --}}
+                            <img width="22px" src="/build/images/templates/{{$item->icon}}.png" alt="" class="img-fluid">
                         </div>
-                        <div class="flex-grow-1 ms-2">
-                            <h5 class="card-title mb-1">{{$item->template_name}}</h5>
-                            <p class="text-muted mb-0">{{$item->description}}</p>
-                        </div>
+                        <h3 class="fw-medium link-primary">{{$item->template_name}}</h3>
+                        <p style="height: 3em; overflow: hidden; color:black;" class="card-text customer_name">{{$item->description}}</p>
+                
+                        <a href="{{ route('frontend.free.template.view', ['slug' => $item->slug]) }}" class="btn btn-primary btn-sm">Generate</a>
+                
+                
+                        <ul class="list-inline hstack gap-2 mb-0">
+                            <li class="list-inline-item" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-placement="top" title="View">
+                                {{-- <a href="apps-ecommerce-order-details" class="text-primary d-inline-block">
+                                    <i class="ri-eye-fill fs-16"></i>
+                                </a> --}}
+                            </li>
+                            <li class="list-inline-item edit" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-placement="top" title="Edit">
+                                {{-- <a href="#showModal" data-bs-toggle="modal" class="text-primary d-inline-block edit-item-btn">
+                                    <i class="ri-pencil-fill fs-16"></i>
+                                </a> --}}
+                            </li>
+                            <li class="list-inline-item" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-placement="top" title="Remove">
+                                {{-- <a class="text-danger d-inline-block remove-item-btn" data-bs-toggle="modal" href="#deleteOrder">
+                                    <i class="ri-delete-bin-5-fill fs-16"></i>
+                                </a> --}}
+                            </li>
+                        </ul>
                     </div>
-                    <a href="{{ route('frontend.free.template.view', ['slug' => $item->slug]) }}" class="btn btn-primary btn-sm">Generate</a>
-                </div>
                 </a>
+                </div>
+           
+       
             </div>
+        
             @endforeach
-        </div><!-- end row -->
-    </div><!-- end col -->
-</div>
-</div>
+    </div>
 </div>
 
             <!-- Start footer -->
