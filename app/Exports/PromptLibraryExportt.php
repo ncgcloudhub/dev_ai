3e78d0e032f2ce8a46e8b63053066b210e5a0217
ifazalam1 <ifaz.alam1@gmail.com>
2024-06-05 17:57:52 +0600

    prompt library export with heading bold

--- app/Exports/PromptLibraryExportt.php
+++ app/Exports/PromptLibraryExportt.php
@@ -3,15 +3,40 @@
 namespace App\Exports;
 
 use App\Models\PromptLibrary;
+use Illuminate\Support\Facades\Schema;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class PromptLibraryExportt implements FromCollection
+class PromptLibraryExportt implements FromCollection, WithHeadings, WithStyles
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
     public function collection()
     {
         return PromptLibrary::all();
+    }
+
+    /**
+     * @return array
+     */
+    public function headings(): array
+    {
+        // Get all column names for the PromptLibrary table
+        return Schema::getColumnListing((new PromptLibrary)->getTable());
+    }
+
+    /**
+     * @param Worksheet $sheet
+     * @return array
+     */
+    public function styles(Worksheet $sheet)
+    {
+        return [
+            // Style the first row (headings row) as bold
+            1 => ['font' => ['bold' => true]],
+        ];
     }
 }
