9af25ab83ee5b37ed8677f68d71aee1be72a5649
ifazalam1 <ifaz.alam1@gmail.com>
2024-06-05 20:04:27 +0600

    prompt category view fix

--- resources/views/backend/prompt_library/category.blade.php
+++ resources/views/backend/prompt_library/category.blade.php
@@ -6,58 +6,84 @@
 @slot('title') Prompt Category  @endslot
 @endcomponent
 
-<div class="col-xxl-6">
+<div class="row">
+    <div class="col-xxl-6">
+        <div class="card">
    
-           
-    <div class="card">
-        <div class="card-header align-items-center d-flex">
-            <h4 class="card-title mb-0 flex-grow-1">Prompt Library Category Add</h4>
-        </div><!-- end card header -->
+            <div class="card-body">
+                <table id="alternative-pagination" class="table responsive align-middle table-hover table-bordered" style="width:100%">
+                    <thead>
+                        <tr>
+                            <th scope="col">Sl.</th>
+                            <th scope="col">Category Name</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @php
+                            $sl = 1; // Initialize the variable outside the loop
+                        @endphp
+                        @foreach ($categories as $item)
+                        <tr>
+                            <td>{{ $sl++ }}</td> <!-- Increment the variable and display its value -->
+                            <td>{{ $item->category_name }}</td>    
+                            <td>
+                                <div class="form-check form-switch form-switch-md" dir="ltr">
 
-        <div class="card-body">
+                                    <a href="{{route('prompt.category.edit',$item->id)}}" class="btn btn-primary btn-sm waves-effect waves-light d-inline-block" data-bs-toggle="tooltip" data-bs-placement="top" title="Edit"><i class="ri-edit-2-fill"></i></a>
 
-            {{-- @include('admin.layouts.alerts') --}}
+                                    <a href="{{route('prompt.category.delete',$item->id)}}" onclick="return confirm('Are you sure you want to delete this Customer')" class="btn btn-danger btn-sm waves-effect waves-light d-inline-block" data-bs-toggle="tooltip" data-bs-placement="top" title="Delete"><i class="ri-delete-bin-line"></i></a>
 
-            <div class="live-preview">
-                <form  action="{{ route('prompt.category.store') }}" method="post" class="row g-3">
-                    @csrf
-
-                    <div class="form-floating">
-                        <input type="text" name="category_name" class="form-control" id="category_name" placeholder="Enter Category">
-                        <label for="category_name">Category</label>
-                    </div>
-
-                    <div class="form-floating">
-                        <input type="text" name="category_icon" class="form-control" id="category_icon" placeholder="Enter Icon">
-                        <label for="icon">Enter Icon</label>
-                    </div>
-                                    
-                    <div class="col-12">
-                        <div class="text-end">
-                            <button class="btn btn-rounded btn-primary mb-2">Save</button>
-                        </div>
-                    </div>
-                </form>
+                                </div>
+                            </td>    
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
             </div>
-      
+
         </div>
     </div>
-   
- <div class="card">
+
+    <div class="col-xxl-6">
     
-        <div class="card-body pt-0">
-
-            <h6 class="text-uppercase fw-semibold mt-4 mb-3 text-muted">Category List</h6>
-            <ul class="list-group">
-                @foreach ($categories as $item)
-                <li class="list-group-item"><i class="{{$item->category_icon}}"></i> {{$item->category_name}}</li>
-                @endforeach
-            </ul>
-
-        </div><!-- end cardbody -->
-    </div> 
- 
+        <div class="card">
+            <div class="card-header align-items-center d-flex">
+                <h4 class="card-title mb-0 flex-grow-1">Prompt Library Category Add</h4>
+            </div><!-- end card header -->
+    
+            <div class="card-body">
+    
+                {{-- @include('admin.layouts.alerts') --}}
+    
+                <div class="live-preview">
+                    <form  action="{{ route('prompt.category.store') }}" method="post" class="row g-3">
+                        @csrf
+    
+                        <div class="form-floating">
+                            <input type="text" name="category_name" class="form-control" id="category_name" placeholder="Enter Category">
+                            <label for="category_name">Category</label>
+                        </div>
+    
+                        <div class="form-floating">
+                            <input type="text" name="category_icon" class="form-control" id="category_icon" placeholder="Enter Icon">
+                            <label for="icon">Enter Icon</label>
+                        </div>
+    
+                        <div class="col-12">
+                            <div class="text-end">
+                                <button class="btn btn-rounded btn-primary mb-2">Save</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+    
+            </div>
+        </div>
+    </div>
+    
 </div>
+
 
 
 @endsection
