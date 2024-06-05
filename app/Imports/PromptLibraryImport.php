2b6e78b3aca9307298cc9e1aa51d9b40fc6a2489
ifazalam1 <ifaz.alam1@gmail.com>
2024-06-05 17:58:38 +0600

    prompt library import

--- app/Imports/PromptLibraryImport.php
+++ app/Imports/PromptLibraryImport.php
@@ -4,21 +4,27 @@ namespace App\Imports;
 
 use App\Models\PromptLibrary;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
-class PromptLibraryImport implements ToModel
+class PromptLibraryImport implements ToModel, WithHeadingRow
 {
     /**
-    * @param array $row
-    *
-    * @return \Illuminate\Database\Eloquent\Model|null
-    */
+     * @param array $row
+     *
+     * @return \Illuminate\Database\Eloquent\Model|null
+     */
     public function model(array $row)
     {
         return new PromptLibrary([
-            'prompt_name'     => $row[1],
-            'icon'   => $row[3], 
-            'category_id'   => $row[4], 
-            'actual_prompt'   => $row[6], 
-       ]);
+            'prompt_name'    => $row['prompt_name'],
+            'slug'           => $row['slug'],
+            'icon'           => $row['icon'],
+            'category_id'    => $row['category_id'],
+            'description'    => $row['description'],
+            'actual_prompt'  => $row['actual_prompt'],
+            'created_at'     => $row['created_at'],
+            'updated_at'     => $row['updated_at'],
+            'sub_category_id' => $row['sub_category_id'],
+        ]);
     }
 }
