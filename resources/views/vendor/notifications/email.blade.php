814f798e290691c13870c0de8807b120f91dd69d
ifazalam1 <ifaz.alam1@gmail.com>
2024-06-12 15:52:13 +0600

    email confirmation link clickable

--- resources/views/vendor/notifications/email.blade.php
+++ resources/views/vendor/notifications/email.blade.php
@@ -42,6 +42,11 @@
             margin-bottom: 10px;
             text-align: center;
         }
+        .content a {
+            color: #2c22b8;
+            word-break: break-all;
+            overflow-wrap: anywhere;
+        }
         .button {
             display: block;
             width: fit-content;
@@ -70,9 +75,10 @@
         <div class="content">
             <p>Hello Creative Mind,</p>
             <p>We're thrilled to have you on board! To get started, please verify your account by clicking the link below. This will confirm your email address and activate your account.</p>
-            <a href="{{$actionUrl}}" class="button">Verify Your Account</a>
+            <a style="color: white" href="{{$actionUrl}}" class="button">Verify Your Account</a>
             <p>If the button above doesn't work, no worries! You can copy and paste the following link into your browser:</p>
-            <p><code>{{$actionUrl}}</code></p>
+            <p><a href="{{$actionUrl}}">{{$actionUrl}}</a></p>
+
             <p>If you're prompted to log in, simply enter your username or email, along with your password, and you'll be all set.</p>
             <p>If you have any questions or need assistance, don't hesitate to reach out. We're here to help!</p>
             <p>Happy Creating,<br>The Clever Creator Team</p>
