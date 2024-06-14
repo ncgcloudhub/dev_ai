89db72fdfda7fc0779c8483a054683fd02e56aad
Fahmid Home <fahmidh26@gmail.com>
2024-06-14 20:06:45 +0600

    user reward after referred email verified

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\Referral;
+use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
@@ -21,6 +23,15 @@ class VerifyEmailController extends Controller
 
         if ($request->user()->markEmailAsVerified()) {
             event(new Verified($request->user()));
+            
+            // Check if the user was referred
+            $referral = Referral::where('referral_id', $request->user()->id)->first();
+            if ($referral) {
+                // Credit the referrer
+                $referrer = User::find($referral->referrer_id);
+                $referrer->increment('credits_left', 10);
+                $referral->update(['status' => 'completed']);
+            }
         }
 
         return redirect()->intended(RouteServiceProvider::HOME.'?verified=1');
