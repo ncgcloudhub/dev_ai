42e1e8fa80ae6973902b6ecffab8fdd8a29eb0e5
ifazalam1 <ifaz.alam1@gmail.com>
2024-06-20 19:49:57 +0600

    topbar welcome and then the name for both admin and user

--- resources/views/user/layouts/topbar.blade.php
+++ resources/views/user/layouts/topbar.blade.php
@@ -163,7 +163,7 @@
                     </button>
                     <div class="dropdown-menu dropdown-menu-end">
                         <!-- item-->
-                        <h6 class="dropdown-header">Welcome!</h6>
+                        <h6 class="dropdown-header">Welcome {{Auth::user()->name}}!</h6>
                         <a class="dropdown-item" href="{{ route('edit.profile') }}"><i class="mdi mdi-account-circle text-muted fs-16 align-middle me-1"></i> <span class="align-middle">Profile</span></a>
                         
                         <a class="dropdown-item" href="{{route('user.dashboard')}}" target="_blank"><i class="mdi mdi-home text-muted fs-16 align-middle me-1"></i> <span class="align-middle">Dashboard</span></a>
