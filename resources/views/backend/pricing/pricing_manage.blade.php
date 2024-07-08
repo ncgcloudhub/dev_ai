8535393e8b6675492d5a28f2997a811c236b90de
Fahmid <fahmidh26@gmail.com>
2024-07-08 13:54:13 +0600

    Pricing Plan Header Admin

--- resources/views/backend/pricing/pricing_manage.blade.php
+++ resources/views/backend/pricing/pricing_manage.blade.php
@@ -45,16 +45,17 @@
         @foreach ($monthlyPlans as $item)
             <div class="col-xxl-3 col-lg-6 month">
                 <div class="card pricing-box ribbon-box right">
+                    <div class="card-header">
+                        <h4 class="card-title mb-0">{{$item->title}}</h4>
+                    </div>
                     <div class="card-body bg-light m-2 p-4">
                         @if ($item->popular === 'yes')
                             <div class="ribbon-two ribbon-two-success"><span>Popular</span></div>
                         @else
                         @endif
                         
-                        <div class="d-flex align-items-center mb-3">
-                            <div class="flex-grow-1">
-                                <h5 class="mb-0 fw-semibold">{{$item->title}}</h5>
-                            </div>
+                        <div class="d-flex align-items-center mb-0">
+                         
                             <div class="ms-auto">
                                 @if ($item->discount_type == 'percentage')
                                     <h2 class="month mb-0"><small class="fs-16"><del>${{ $item->price }}</del></small> ${{ $item->discounted_price }}</h2>
@@ -258,16 +259,17 @@
         
         <div class="col-xxl-3 col-lg-6 annual">
             <div class="card pricing-box ribbon-box right">
+                <div class="card-header">
+                    <h4 class="card-title mb-0">{{$item->title}}</h4>
+                </div>
                 <div class="card-body bg-light m-2 p-4">
                     @if ($item->popular === 'yes')
                         <div class="ribbon-two ribbon-two-success"><span>Popular</span></div>
                     @else
                     @endif
                     
-                    <div class="d-flex align-items-center mb-3">
-                        <div class="flex-grow-1">
-                            <h5 class="mb-0 fw-semibold">{{$item->title}}</h5>
-                        </div>
+                    <div class="d-flex align-items-center mb-0">
+                      
                         <div class="ms-auto">
                             <h2 class="annual mb-0"><small class="fs-16"><del>${{$item->price}}</del></small> ${{$item->discounted_price}}</h2>
                             @if ($item->discount_type == 'percentage')
