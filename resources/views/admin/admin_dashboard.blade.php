4550f8e3cb5e7e91beee3d8ab3b343c078aaa37a
ifazalam1 <ifaz.alam1@gmail.com>
2024-07-11 16:39:50 +0600

    chat removed from admin dashboard and favoutite images added

--- resources/views/admin/admin_dashboard.blade.php
+++ resources/views/admin/admin_dashboard.blade.php
@@ -390,7 +390,7 @@
             <div class="row">
             
                 {{-- CHAT START--}}
-                <div class="col-xxl-6 col-lg-6">
+                {{-- <div class="col-xxl-6 col-lg-6">
                     <div class="card card-height-100">
                         <div class="card-header align-items-center d-flex">
                             <h4 class="card-title mb-0 flex-grow-1">Chat</h4>
@@ -449,7 +449,7 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> --}}
 
                 {{-- CHAT END --}}
                 {{-- ------------------------------------------------------------------------------------------------------- --}}
@@ -481,6 +481,43 @@
                                             <img height="50px" width="50px" class="gallery-img img-fluid mx-auto" src="{{ $image->image_url }}" alt="{{ $image->prompt }}" />
                                         </td>
                                         <td>{{ $image->likes_count }}</td>
+                                    </tr>
+                                    @endforeach
+                                </tbody><!-- end tbody -->
+                            </table>
+                            
+                        </div>
+                    </div> <!-- .card-->
+                </div> <!-- .col-->
+
+
+                <div class="col-xxl-6 col-lg-6">
+                    <div class="card">
+                        <div class="card-header align-items-center d-flex">
+                            <h4 class="card-title mb-0 flex-grow-1">Favourite Images  <span class="badge bg-success-subtle text-success">{{ $totalFav }}
+                            </span></h4>
+                        
+                        </div><!-- end card header -->
+
+                        <div class="card-body">
+                            <table id="example" class="table table-bordered dt-responsive nowrap table-striped align-middle" style="width:100%">
+                                <thead>
+                                    <tr>
+                                        <th scope="col" width="5%">#</th>
+                                        <th scope="col">Image</th>
+                                        <th scope="col">Favourites</th>
+                                    
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                
+                                    @foreach($favImages as $image)
+                                    <tr>
+                                        <td width="5%">{{ $loop->iteration }}</td>
+                                        <td>
+                                            <img height="50px" width="50px" class="gallery-img img-fluid mx-auto" src="{{ $image->image_url }}" alt="{{ $image->prompt }}" />
+                                        </td>
+                                        <td>{{ $image->favorites_count }}</td>
                                     </tr>
                                     @endforeach
                                 </tbody><!-- end tbody -->
