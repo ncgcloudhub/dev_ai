f5c2c85da8ed919f25576bef4a8cef561e6eec3e
Fahmid Home <fahmidh26@gmail.com>
2024-08-25 01:24:38 +0600

    titles added

--- resources/views/admin/layouts/master-without-nav.blade.php
+++ resources/views/admin/layouts/master-without-nav.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>Clever Creator</title>
+    <title>@yield('title') | {{ $siteSettings->title }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="Clever Creator" name="description" />
     <meta content="Clever Creator" name="keywords" />
