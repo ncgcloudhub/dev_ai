7715fce49623ed7f6139644a5be34941969f89e0
ifazalam1 <ifaz.alam1@gmail.com>
2024-08-27 13:41:41 +0600

    user topbar changes, Unnecessary codes removed

--- resources/views/user/layouts/topbar.blade.php
+++ resources/views/user/layouts/topbar.blade.php
@@ -94,25 +94,7 @@
                 </div>
             </div>
 
-            
-
             <div class="d-flex align-items-center">
-                <!-- Search Dropdown (Mobile) -->
-                <div class="dropdown d-md-none topbar-head-dropdown header-item">
-                    <button type="button" class="btn btn-icon btn-topbar btn-ghost-secondary rounded-circle" id="page-header-search-dropdown" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <i class="bx bx-search fs-22"></i>
-                    </button>
-                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-end p-0" aria-labelledby="page-header-search-dropdown">
-                        <form class="p-3">
-                            <div class="form-group m-0">
-                                <div class="input-group">
-                                    <input type="text" class="form-control" placeholder="Search ..." aria-label="Recipient's username">
-                                    <button class="btn btn-primary" type="submit"><i class="mdi mdi-magnify"></i></button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
 
                 <!-- Fullscreen Button -->
                 <div class="ms-1 header-item d-none d-sm-flex">
@@ -127,6 +109,7 @@
                         <i class='bx bx-moon fs-22'></i>
                     </button>
                 </div>
+                
                 @include('user.layouts.activity_log')
                 <!-- User Dropdown -->
                 <div class="dropdown ms-sm-3 header-item topbar-user">
@@ -156,29 +139,7 @@
     </div>
 </header>
 
-<!-- Remove Notification Modal -->
-<div id="removeNotificationModal" class="modal fade zoomIn" tabindex="-1" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered">
-        <div class="modal-content">
-            <div class="modal-header">
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" id="NotificationModalbtn-close"></button>
-            </div>
-            <div class="modal-body">
-                <div class="mt-2 text-center">
-                    <lord-icon src="https://cdn.lordicon.com/gsqxdxog.json" trigger="loop" colors="primary:#f7b84b,secondary:#f06548" style="width:100px;height:100px"></lord-icon>
-                    <div class="mt-4 pt-2 fs-15 mx-4 mx-sm-5">
-                        <h4>Are you sure?</h4>
-                        <p class="text-muted mx-4 mb-0">Are you sure you want to remove this Notification?</p>
-                    </div>
-                </div>
-                <div class="d-flex gap-2 justify-content-center mt-4 mb-2">
-                    <button type="button" class="btn w-sm btn-light" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn w-sm btn-danger" id="delete-notification">Yes, Delete It!</button>
-                </div>
-            </div>
-        </div><!-- /.modal-content -->
-    </div><!-- /.modal-dialog -->
-</div><!-- /.modal -->
+
 
 <script>
    document.addEventListener('DOMContentLoaded', function() {
