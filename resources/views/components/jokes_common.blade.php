3441ed536573f24f2d325b66e7857e54081d523c
ifazalam1 <ifaz.alam1@gmail.com>
2024-11-06 15:35:18 +0600

    Generated Images giphy

--- resources/views/components/jokes_common.blade.php
+++ resources/views/components/jokes_common.blade.php
@@ -5,12 +5,12 @@
         left: 0;
         width: 100%;
         height: 100%;
-        background: rgba(0, 0, 0, 0.6); /* Semi-transparent overlay */
+        background: rgba(0, 0, 0, 0.3); /* Adjust transparency here */
         display: flex;
         align-items: center;
         justify-content: center;
         color: #fff;
-        z-index: 1050; /* Ensures overlay is above other elements */
+        z-index: 1050;
     }
     .magic-ball-content {
         display: flex;
@@ -18,7 +18,7 @@
         align-items: center;
     }
     .magic-ball-gif {
-        max-width: 100px;
+        max-width: 90%;
         margin-bottom: 20px;
     }
     .d-none {
@@ -29,12 +29,10 @@
 <!-- resources/views/components/magic-ball.blade.php -->
 <div id="magic-ball" class="magic-ball-overlay d-none">
     <div class="magic-ball-content">
-        <img src="{{ asset('backend/giphy.gif') }}" alt="Loading..." class="magic-ball-gif">
+        <img src="{{ asset('backend/giphy2.gif') }}" alt="Loading..." class="magic-ball-gif">
         <p id="joke-text">Fetching a joke...</p>
     </div>
 </div>
-
-<!-- Magic Ball Styles -->
 
 <script>
     function fetchJoke(category) {
@@ -48,12 +46,11 @@
     }
 
     function showMagicBall(category) {
-        $('#magic-ball').removeClass('d-none'); // Show overlay
-        fetchJoke(category); // Fetch joke based on category
+        $('#magic-ball').removeClass('d-none');
+        fetchJoke(category);
     }
 
     function hideMagicBall() {
-        $('#magic-ball').addClass('d-none'); // Hide overlay
+        $('#magic-ball').addClass('d-none');
     }
 </script>
-
