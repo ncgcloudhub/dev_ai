036ba91935d860d9712996c60d3af6f9d2b8c22f
ifazalam1 <ifaz.alam1@gmail.com>
2024-11-26 13:28:21 +0600

    faq in 2 columns in frontend

--- resources/views/frontend/body/faq.blade.php
+++ resources/views/frontend/body/faq.blade.php
@@ -4,8 +4,7 @@
             <div class="col-lg-8">
                 <div class="text-center mb-5">
                     <h3 class="mb-3 fw-semibold">Frequently Asked Questions</h3>
-                    <p class="text-muted mb-4 ff-secondary">If you can not find answer to your question in our
-                        FAQ, you can
+                    <p class="text-muted mb-4 ff-secondary">If you cannot find an answer to your question in our FAQ, you can
                         always contact us or email us. We will answer you shortly!</p>
 
                     <div class="hstack gap-2 justify-content-center">
@@ -20,8 +19,8 @@
         </div>
         <!-- end row -->
 
-        <div class="row g-lg-8 g-4 d-flex justify-content-center">
-            <div class="col-lg-8">
+        <div class="row g-lg-8 g-4">
+            <div class="col-lg-6">
                 <div class="d-flex align-items-center mb-2">
                     <div class="flex-shrink-0 me-1">
                         <i class="ri-question-line fs-24 align-middle text-success me-1"></i>
@@ -30,15 +29,15 @@
                         <h5 class="mb-0 fw-semibold">General Questions</h5>
                     </div>
                 </div>
-                <div class="accordion custom-accordionwithicon custom-accordion-border accordion-border-box" id="genques-accordion">
-                    @foreach ($faqs as $index => $item)
+                <div class="accordion custom-accordionwithicon custom-accordion-border accordion-border-box" id="genques-accordion-1">
+                    @foreach ($faqs->slice(0, ceil($faqs->count() / 2)) as $index => $item)
                         <div class="accordion-item">
                             <h2 class="accordion-header" id="genques-heading{{$index}}">
                                 <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#genques-collapse{{$index}}" aria-expanded="false" aria-controls="genques-collapse{{$index}}">
                                    {{$item->question}}
                                 </button>
                             </h2>
-                            <div id="genques-collapse{{$index}}" class="accordion-collapse collapse" aria-labelledby="genques-heading{{$index}}" data-bs-parent="#genques-accordion">
+                            <div id="genques-collapse{{$index}}" class="accordion-collapse collapse" aria-labelledby="genques-heading{{$index}}" data-bs-parent="#genques-accordion-1">
                                 <div class="accordion-body ff-secondary">
                                     {!! $item->answer !!} 
                                 </div>
@@ -46,12 +45,35 @@
                         </div>
                     @endforeach
                 </div>
-                
                 <!--end accordion-->
-
             </div>
             <!-- end col -->
-           
+
+            <div class="col-lg-6">
+                <div class="d-flex align-items-center mb-2">
+                    <div class="flex-shrink-0 me-1">
+                        <i class="fs-24 align-middle text-success me-1"></i>
+                    </div>
+                </div>
+                <div class="accordion custom-accordionwithicon custom-accordion-border accordion-border-box" id="genques-accordion-2">
+                    @foreach ($faqs->slice(ceil($faqs->count() / 2)) as $index => $item)
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="genques-heading{{$index + ceil($faqs->count() / 2)}}">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#genques-collapse{{$index + ceil($faqs->count() / 2)}}" aria-expanded="false" aria-controls="genques-collapse{{$index + ceil($faqs->count() / 2)}}">
+                                   {{$item->question}}
+                                </button>
+                            </h2>
+                            <div id="genques-collapse{{$index + ceil($faqs->count() / 2)}}" class="accordion-collapse collapse" aria-labelledby="genques-heading{{$index + ceil($faqs->count() / 2)}}" data-bs-parent="#genques-accordion-2">
+                                <div class="accordion-body ff-secondary">
+                                    {!! $item->answer !!} 
+                                </div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+                <!--end accordion-->
+            </div>
+            <!-- end col -->
         </div>
         <!-- end row -->
     </div>
