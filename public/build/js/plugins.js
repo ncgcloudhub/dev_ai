10a5b7044e389eea5fc28aa35b92bb3683742d2d
ifazalam1 <ifaz.alam1@gmail.com>
2025-01-09 15:27:12 +0600

    Refactor script loading condition for improved readability

--- public/build/js/plugins.js
+++ public/build/js/plugins.js
@@ -8,8 +8,18 @@ File: Common Plugins Js File
 */
 
 //Common plugins
-if(document.querySelectorAll("[toast-list]") || document.querySelectorAll('[data-choices]') || document.querySelectorAll("[data-provider]")){ 
-  document.writeln("<script type='text/javascript' src='https://cdn.jsdelivr.net/npm/toastify-js'></script>");
-  document.writeln("<script type='text/javascript' src='build/libs/choices.js/public/assets/scripts/choices.min.js'></script>");
-  document.writeln("<script type='text/javascript' src='build/libs/flatpickr/flatpickr.min.js'></script>");    
+if (
+    document.querySelectorAll("[toast-list]") ||
+    document.querySelectorAll("[data-choices]") ||
+    document.querySelectorAll("[data-provider]")
+) {
+    document.writeln(
+        "<script type='text/javascript' src='https://cdn.jsdelivr.net/npm/toastify-js'></script>"
+    );
+    document.writeln(
+        "<script type='text/javascript' src='/build/libs/choices.js/public/assets/scripts/choices.min.js'></script>"
+    );
+    document.writeln(
+        "<script type='text/javascript' src='/build/libs/flatpickr/flatpickr.min.js'></script>"
+    );
 }
