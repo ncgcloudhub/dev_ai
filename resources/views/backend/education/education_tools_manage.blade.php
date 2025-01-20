5ee0c306d2bc7c52cfcc76abee38c3a916b5de6f
ifazalam1 <ifaz.alam1@gmail.com>
2025-01-20 13:19:44 +0600

    Enhance Education Tools layout with similar tools section and improved navigation

--- resources/views/backend/education/education_tools_manage.blade.php
+++ resources/views/backend/education/education_tools_manage.blade.php
@@ -32,7 +32,37 @@
     </div>
 </section>
 
-    <div class="container">
+<div class="d-flex">
+    <div class="col-2 card mb-3 mt-3 border-color-purple">
+        <div class="card-body">
+            <div class="d-flex mb-3 align-items-center">
+                <h6 class="card-title mb-0 flex-grow-1 gradient-text-1-bold">Similar Tools</h6>
+                <a href="{{route('manage.education.tools')}}" class="ms-auto">
+                    <span class="badge badge-gradient-purple">More Tools</span>
+                </a>
+            </div>
+            
+            <ul class="list-unstyled vstack gap-3 mb-0">
+                    <li>
+                        <div class="d-flex align-items-center">
+                            <div class="flex-shrink-0">
+                                <img src="" alt="" class="avatar-xs rounded-3">
+                            </div>
+                            <div class="flex-grow-1 ms-2">
+                              
+                                <h6 class="mb-1"><a href="" class="gradient-text-1">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam nemo architecto sit debitis quae maiores soluta corporis, optio explicabo officia aperiam sequi doloribus modi quo facere deserunt a mollitia voluptatibus eos. Inventore iusto esse repellat nobis ullam nesciunt veritatis rerum maxime cum voluptatem perferendis incidunt vitae et repudiandae ut aliquam, quas ea iste, excepturi facilis voluptas. Laborum amet voluptate nulla facere magni, libero vitae molestias, ab nam dicta ratione voluptatem esse tenetur numquam quibusdam, accusamus dolorem. Id facere distinctio, rerum atque, nobis placeat, doloribus dolore laborum officia sit explicabo vero deserunt. Voluptate suscipit facere adipisci quos minus ex optio. Voluptatum!</a></h6>
+                                <p class="text-muted mb-0">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim assumenda, at voluptas praesentium adipisci facere sit voluptatibus obcaecati dolorum officiis!</p>
+                            </div>
+                         
+                        </div>
+                    </li>
+            </ul>
+        </div>
+    </div>
+    
+
+    <div class="col-8 px-3">
+        
         <div class="row justify-content-center">
             <form>
                 <div class="row g-3 justify-content-center my-3">
@@ -152,7 +182,35 @@
         </div>
     </div>
 
+    <div class="col-2 card mb-3 mt-3 border-color-purple">
+        <div class="card-body">
+            <div class="d-flex mb-3 align-items-center">
+                <h6 class="card-title mb-0 flex-grow-1 gradient-text-1-bold">Similar Tools</h6>
+                <a href="{{route('manage.education.tools')}}" class="ms-auto">
+                    <span class="badge badge-gradient-purple">More Tools</span>
+                </a>
+            </div>
+            
+            <ul class="list-unstyled vstack gap-3 mb-0">
+                    <li>
+                        <div class="d-flex align-items-center">
+                            <div class="flex-shrink-0">
+                                <img src="" alt="" class="avatar-xs rounded-3">
+                            </div>
+                            <div class="flex-grow-1 ms-2">
+                              
+                                <h6 class="mb-1"><a href="" class="gradient-text-1">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cupiditate quidem exercitationem porro veritatis accusantium dolore reiciendis officiis non, ea eius officia tenetur corrupti, ipsa consequuntur voluptatibus! Rerum accusantium molestias ad. Molestias tempore unde nesciunt officia illo rerum. Dolore reiciendis soluta odit iusto officiis facere illum, quaerat non quibusdam maiores veritatis laudantium libero id tenetur quod ducimus exercitationem ad eum dolores necessitatibus harum quas voluptas. Consectetur sit deleniti, laudantium corporis ab impedit saepe possimus aliquam laborum id vitae minima quo! Incidunt tempora quas consequuntur reprehenderit commodi, dolor explicabo repellat fuga voluptatem adipisci ipsa hic facilis id dolores, optio labore nesciunt corporis!</a></h6>
+                                <p class="text-muted mb-0">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim assumenda, at voluptas praesentium adipisci facere sit voluptatibus obcaecati dolorum officiis!</p>
+                            </div>
+                         
+                        </div>
+                    </li>
+            </ul>
+        </div>
+    </div>
+</div>
 
+   
 <button onclick="topFunction()" class="btn btn-danger btn-icon landing-back-top" id="back-to-top">
     <i class="ri-arrow-up-line"></i>
 </button>
