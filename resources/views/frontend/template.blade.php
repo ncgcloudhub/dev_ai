ee74fca8d148041cc8b9a85484cc5db2f4e0f01f
ifazalam1 <ifaz.alam1@gmail.com>
2025-01-27 15:38:03 +0600

    Change navigation tabs from 'nav-tabs' to 'nav-pills' for improved styling consistency

--- resources/views/frontend/template.blade.php
+++ resources/views/frontend/template.blade.php
@@ -54,7 +54,7 @@
         </form>
     </div>
 
-    <ul class="nav nav-tabs nav-tabs-custom nav-success mb-3 justify-content-center" role="tablist">
+    <ul class="nav nav-tabs nav-pills nav-tabs-custom nav-success mb-3 justify-content-center" role="tablist">
         <li class="nav-item" id="category-tour">
             <a class="nav-link n1 active All py-3" data-bs-toggle="tab" id="All"
                 href="#home1" role="tab" aria-selected="true">
