331ec2bbfa4e5d29fc21de4e9f8c98642cc61498
ifazalam1 <ifaz.alam1@gmail.com>
2025-04-12 16:15:34 +0600

    Export Manage Image

---
+++ app/Exports/GeneratedImagesExport.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\DalleImageGenerate;
+use App\Models\GeneratedImage;
+use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+
+class GeneratedImagesExport implements FromCollection, WithHeadings
+{
+    public function collection()
+    {
+        return DalleImageGenerate::with('user')->get()->map(function ($image) {
+            return [
+                'ID' => $image->id,
+                'Prompt' => $image->prompt,
+                'User' => $image->user ? $image->user->name : 'N/A',
+                'Image URL' => asset($image->image_url),
+                'Status' => $image->status,
+                'Created At' => $image->created_at->format('Y-m-d H:i:s'),
+            ];
+        });
+    }
+
+    public function headings(): array
+    {
+        return [
+            'ID', 'Prompt', 'User', 'Image URL', 'Status', 'Created At'
+        ];
+    }
+}
