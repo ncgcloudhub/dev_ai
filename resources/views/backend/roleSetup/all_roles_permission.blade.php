0ae27a5cf525d9626ef947fecc689772607e386b
ifazalam1 <ifaz.alam1@gmail.com>
2025-04-27 14:24:22 +0600

    feat: enhance role management UI with icon buttons for add, edit, and delete actions

--- resources/views/backend/roleSetup/all_roles_permission.blade.php
+++ resources/views/backend/roleSetup/all_roles_permission.blade.php
@@ -14,7 +14,7 @@
     <div class="col-lg-12">
         <div class="card">
             <div class="card-header">
-                <h5 class="card-title mb-1">Role in Permission <a href="{{ route('add.roles.permission') }}" class="btn gradient-btn-9">Add</a></h5>
+                <h5 class="card-title mb-1">Role in Permission <a href="{{ route('add.roles.permission') }}" class="btn gradient-btn-add" title="Add Role In Permission"><i class="{{$buttonIcons['add']}}"></i></a></h5>
                
             </div>
 
@@ -58,13 +58,13 @@
                            
                             <td>
                                 <li class="list-inline-item edit" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-placement="top" title="Edit">
-                                    <a href="{{ route('admin.edit.roles',$item->id) }}" class="text-primary d-inline-block edit-item-btn">
-                                        <i class="ri-pencil-fill fs-16"></i>
+                                    <a href="{{ route('admin.edit.roles',$item->id) }}" class="gradient-btn-edit d-inline-block">
+                                        <i class="{{$buttonIcons['edit']}}"></i>
                                     </a>
                                 </li>
                                 <li class="list-inline-item" data-bs-toggle="tooltip" data-bs-trigger="hover" data-bs-placement="top" title="Remove">
-                                    <a class="text-danger d-inline-block remove-item-btn" href="{{ route('admin.delete.roles',$item->id) }}" onclick="return confirm('Are you sure you want to delete this Prompt')">
-                                        <i class="ri-delete-bin-5-fill fs-16"></i>
+                                    <a class="gradient-btn-delete d-inline-block" href="{{ route('admin.delete.roles',$item->id) }}" onclick="return confirm('Are you sure you want to delete this Prompt')">
+                                        <i class="{{$buttonIcons['delete']}}"></i>
                                     </a>
                                 </li>
                             </td>
