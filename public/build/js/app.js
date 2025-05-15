b6179f17b2a45217d9e3b501fbfb8846984cc85d
ifazalam1 <ifaz.alam1@gmail.com>
2025-05-15 21:51:00 +0600

    fix bell.svg path in app.js

--- public/build/js/app.js
+++ public/build/js/app.js
@@ -1475,7 +1475,7 @@ File: Main Js File
                             elem.innerHTML +=
                                 '<div class="empty-notification-elem">\
 							<div class="w-25 w-sm-50 pt-3 mx-auto">\
-								<img src="build/images/svg/bell.svg" class="img-fluid" alt="user-pic">\
+								<img src="/build/images/svg/bell.svg" class="img-fluid" alt="user-pic">\
 							</div>\
 							<div class="text-center pb-5 mt-2">\
 								<h6 class="fs-18 fw-semibold lh-base">Hey! You have no any notifications </h6>\
